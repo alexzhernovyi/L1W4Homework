5feb7838013daefad3efd2b3e7981bb70716d1f4
Alex Z <azhernovyi@gmail.com>
2018-07-24 20:37:03 +0300

    Write a JavaScript function to get the value of the href, hreflang, rel, target, and type attributes of the specified link.

---
+++ 2 task/style.js
@@ -0,0 +1,13 @@
+function getAttributes() {
+    var a = document.getElementById("w3resource").href;
+    alert('Write a JavaScript function to get the value of the href : '+a);
+    var b = document.getElementById("w3resource").hreflang;
+    alert('Write a JavaScript function to get the value of the hreflang : '+b);
+    var c = document.getElementById("w3resource").rel;
+    alert('Write a JavaScript function to get the value of the rel : '+c);
+    var d = document.getElementById("w3resource").target;
+    alert('Write a JavaScript function to get the value of the target : '+d);
+    var i = document.getElementById("w3resource").type;
+    alert('Write a JavaScript function to get the value of the target : '+i);
+
+}
