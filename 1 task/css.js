2665808e1912394c177b241d01e0af54cb798327
Alex Z <azhernovyi@gmail.com>
2018-07-24 20:36:45 +0300

    Modify the style of the paragraph text through javascript code

---
+++ 1 task/css.js
@@ -0,0 +1,12 @@
+function js_style() {
+    text.style.width = 250 + 'px';
+    text.style.height = 90 + 'px';
+    text.style.fontSize = '25pt';
+    text.style.color = '#6476b7';
+    text.style.fontFamily = 'Times New Roman';
+
+}
+
+
+
+
