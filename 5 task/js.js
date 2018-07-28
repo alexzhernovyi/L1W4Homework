6ee1e95d3e6bbcf7abf6b23848798dd75630f644
Alex Z <azhernovyi@gmail.com>
2018-07-28 14:41:52 +0300

    Write a simple SPA app using only one index.html file and history object.

---
+++ 5 task/js.js
@@ -0,0 +1,55 @@
+var links, updatestate, updatebuttons, contentEL, navEL;
+
+contentEI = document.querySelector('.content');
+navEl = document.querySelector('.nav'); 
+
+links = {
+    main: 'The <strong>Main</strong> page',
+    about: 'The <strong>About</strong> page',
+    downloads:'The <strong>Downloads</strong> page'
+}
+
+updatestate = function (state) {
+    if(!state) return;
+    contentEI.innerHTML = links[state.page];
+    updatebuttons(state);
+
+}
+
+updatebuttons = function(state){
+    [].slice.call(navEL.querySelectorAll('A'))
+    .forEach(function(e){
+        var classList = e.parentNode.classList
+        state.page === e.getAttribute('href')
+        ? classList.add('active')
+        : classList.remove('active');
+    })
+
+
+
+}
+
+navEL.addEventListener('click', function(e){
+    e.preventDefault();
+    var state;
+
+    if(e.target.tagName !== 'A') return;
+
+
+    state = {
+        page: e.target.getAttribute('href')
+    }
+
+    history.pushState(state, '', state.page);
+    updatestate(state);
+
+
+})
+window.addEventListener('popstate', function(e){
+    updatestate(state);
+
+
+
+});
+window.addEventListener('hashchange', updatestate);
+window.addEventListener('load', updatestate);
